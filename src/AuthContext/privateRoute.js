587acc3b5d80a49cb011abf691324a9e2e44e6ee
Thiago Porfirio <thiagoporfirio@MacBook-Pro-de-Thiago.local>
2023-09-27 21:29:47 -0300

    adicionando verificacao para user?true

--- src/AuthContext/privateRoute.js
+++ src/AuthContext/privateRoute.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Navigate } from "react-router";
 
 export function PrivateRoute({ children }) {
-  const user = false;
+  const user = localStorage.getItem("user") === "true";
 
   return user ? children : <Navigate to="/" />;
 };
