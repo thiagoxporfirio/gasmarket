7c1872c97db0179bf67f3a3bf5ca9a701a7d2b71
thiago <thiagovporfirio@gmail.com>
2023-09-30 16:42:19 -0300

    adicionado o icone de relatorios e mais modificacoes

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,61 +1,86 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { GiGasStove } from "react-icons/gi";
+import { SlChart } from "react-icons/sl";
+import { Tooltip as ReactTooltip } from "react-tooltip";
 
 import InputMask from "react-input-mask";
 import { toast, ToastContainer } from "react-toastify";
+import { PulseLoader } from "react-spinners";
 import "react-toastify/dist/ReactToastify.css";
-
+import { Link } from "react-router-dom";
 
 export default function Register() {
-  const [formData, setFormData] = useState({
-    nome: "",
-    categoria: "pessoa",
-    endereco: "",
-    telefone: "",
-    valor: "",
-  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [categoria, setCategoria] = useState("residencia"); // Padrão para "Residencia"
+  const [endereco, setEndereco] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [valor, setValor] = useState("");
 
   const formatarValor = (valor) => {
+
     return `R$ ${valor}`;
+
   };
 
+  const handleSelectChange = (e) => {
+
+    setCategoria(e.target.value);
+
+  };
 
   const handleValorChange = (e) => {
+
     const novoValor = e.target.value.replace(/\D/g, ""); // Remove não números
-    setFormData({
-      ...formData,
-      valor: novoValor
-    });
+    setValor(novoValor); // Atualize o estado "valor" diretamente
+
   };
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    try{
-      const response = await fetch("", {
+    try {
+      const formData = {
+        nome: name,
+        categoria: categoria,
+        endereco: endereco,
+        telefone: telefone,
+        valor: valor,
+      };
+
+      const response = await fetch("http://localhost:3001/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(formData),
       });
 
-      if(response.ok){
-        setFormData({
-          nome: "",
-          categoria: "pessoa",
-          endereco: "",
-          telefone: "",
-          valor: "",
-        });
-      }else{
-        toast.error("Error! Verifique se os dados estao corretos.")
+      if (response.ok) {
+
+        setIsLoading(true);
+
+      } else {
+
+        toast.error("Error! Verifique se os dados estao corretos.");
       }
-    }catch(error){
-      console.error("Error ao enviar o FORM:", error)
+    } catch (error) {
+
+      console.error("Error ao enviar o FORM:", error);
+      setIsLoading(false);
+      
+    } finally {
+
+      setName("");
+      setCategoria("residencia");
+      setEndereco("");
+      setTelefone("");
+      setValor("");
+
+      setIsLoading(false); // Certifique-se de definir isLoading como falso, mesmo em caso de erro.
     }
-  }
+  };
 
   return (
     <div className="flex h-screen items-center justify-center bg-gray-100">
@@ -64,14 +89,28 @@ export default function Register() {
           <GiGasStove alt="Logo da Empresa" className="mx-auto w-[60px] h-16" />
           <h1 className="text-2xl font-semibold">Cadastrar cliente</h1>
         </div>
+        <div
+          className="cursor-pointer "
+          style={{ display: "flex", justifyContent: "end" }}
+        >
+          <Link to="/cliente">
+            <SlChart
+              data-tooltip-id="my-tooltip"
+              data-tooltip-content="Pagina de Relatorios"
+            />
+            <ReactTooltip id="my-tooltip" />
+          </Link>
+        </div>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label htmlFor="nome" className="block text-gray-600 font-medium">
               Nome
             </label>
             <input
+              onChange={(e) => setName(e.target.value)}
               type="text"
               id="nome"
+              value={name}
               name="nome"
               className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
             />
@@ -85,12 +124,15 @@ export default function Register() {
             </label>
             <select
               id="categoria"
+              value={categoria}
               name="categoria"
+              onChange={handleSelectChange}
               className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
             >
-              <option value="pessoa">Residencia</option>
-              <option value="rua">Automatica</option>
+              <option value="residencia">Residencia</option>
+              <option value="automatica">Automatica</option>
               <option value="estabelecimento">Estabelecimento</option>
+              <option value="portaria">Portaria</option>
             </select>
           </div>
           <div className="mb-4">
@@ -102,8 +144,10 @@ export default function Register() {
             </label>
             <input
               type="text"
+              value={endereco}
               id="endereco"
               name="endereco"
+              onChange={(e) => setEndereco(e.target.value)}
               className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
               placeholder="Av presidente castelo branco, Centro 453"
             />
@@ -118,8 +162,10 @@ export default function Register() {
             <InputMask
               type="tel"
               id="telefone"
+              value={telefone}
               name="telefone"
               mask="(99) 99999-9999"
+              onChange={(e) => setTelefone(e.target.value)}
               className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
               placeholder="(00) 00000-0000"
             />
@@ -132,7 +178,7 @@ export default function Register() {
               type="text"
               id="valor"
               name="valor"
-              // value={valor}
+              value={valor}
               onChange={handleValorChange}
               className=" w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 justify-start "
               placeholder="0.00"
@@ -143,9 +189,16 @@ export default function Register() {
           <div className="mt-4">
             <button
               type="submit"
-              className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 focus:outline-none"
+              className={`w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 focus:outline-none ${
+                isLoading ? "opacity-50 cursor-wait" : ""
+              }`}
+              disabled={isLoading}
             >
-              Cadastrar
+              {isLoading ? (
+                <PulseLoader color="#ffffff" size={8} />
+              ) : (
+                "Cadastrar"
+              )}
             </button>
           </div>
         </form>
